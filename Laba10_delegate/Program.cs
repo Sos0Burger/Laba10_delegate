6748791ba5876e4a42b9cd43a5498020585b7f09
Ixor <dsv>
2023-03-20 14:02:27 +0300

    Добавьте файлы проекта.

---
+++ Laba10_delegate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Garage garage = new Garage();
+        garage.carList.Add(new Car("Крутая"));
+
+        garage.carList.Add(new Car("Не очень крутая"));
+
+        WasherDelegate washer = new Washer().wash;
+
+        washer(garage.carList[0]);
+        washer(garage.carList[1]);
+
+    }
+}
+class Car
+{
+    public string Name { get; set; }
+
+    public Car(string name)
+    {
+        Name = name;
+    }
+}
+class Garage
+{
+    public List<Car> carList { get; set; }
+
+    public Garage()
+    {
+        carList = new List<Car>();
+    }
+}
+class Washer
+{
+    public void wash(Car car)
+    {
+        Console.WriteLine($"Машина {car.Name} помыта");
+    }
+}
+
+delegate void WasherDelegate(Car car);
